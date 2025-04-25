dc4f09489ca2fd217c036ff4b78a154b29d4f984
euclid8128 <y.kato.534@ms.saitama-u.ac.jp>
2025-04-25 15:56:28 +0900

    add day1 files

---
+++ sample.jl
@@ -0,0 +1,21 @@
+using FastGaussQuadrature, LinearAlgebra
+using Plots
+
+list = []
+f(x) = sqrt(1-x^2)
+err_list = []
+x_list = [i*10 for i in 1:20]
+
+for i in 1:20
+    x,w = gausslegendre(i*10)
+    y = dot(w,f.(x))
+    push!(list,y)
+end
+
+for i in 1:20
+    push!(err_list,list[i]-π/2.0)
+end
+
+gr()
+p = plot(x_list,err_list,st=:scatter,yscale=:log10)
+savefig(p, "sample.pdf")
