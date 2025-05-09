ac0d4427894be13cca8143c178e831bcd2869fa5
euclid8128 <y.kato.534@ms.saitama-u.ac.jp>
2025-05-09 15:04:00 +0900

    renew day2/sample.jl

--- day2/sample.jl
+++ day2/sample.jl
@@ -2,5 +2,5 @@ import Pkg
 Pkg.activate(".")
 using QuadGK
 
-integral, err = quadgk(x -> sqrt(1-x^2),0,1)
-print(integral, err)
+integral, err = quadgk(x -> sqrt(1-x^2),-1,1)
+print(integral,",", err)
